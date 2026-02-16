074da49b179e43f685a26e52b7acca6cca27a915
Zishan2211 <alljubayerzishan@gmail.com>
2026-02-16 23:10:11 +0600

    added new fetures ternary operator

--- script/index.js
+++ script/index.js
@@ -21,24 +21,24 @@ const displayProducts = (products) => {
         pCard.innerHTML = `
        <div class="card w-80 bg-base-100 shadow-xl">
                     <div class="p-6 h-3/4 flex justify-center bg-gray-200">
-                        <img class="h-40 w-40" src="${product.image}" alt="" />
+                        <img class="h-40 w-40" src="${product.image ? product.image : "No Image Available"}" alt="" />
                     </div>
                     <div class="card-body space-y-2">
                         <div class="flex items-center justify-between">
                             <span class="bg-blue-300 text-blue-800 text-xs font-semibold px-2 py-1 rounded">
-                               ${product.category}
+                               ${product.category ? product.category : "No Category"}
                             </span>
                             <div class="flex items-center gap-1 text-sm">
-                                <i class="fa-solid fa-star" style="color: rgba(255, 212, 59, 1);"></i> <span>${product.rating.rate}</span>
-                                <span class="text-gray-400">${product.rating.count}</span>
+                                <i class="fa-solid fa-star" style="color: rgba(255, 212, 59, 1);"></i> <span>${product.rating.rate ? product.rating.rate : "No Rating"}</span>
+                                <span class="text-gray-400">(${product.rating.count ? product.rating.count : "0"})</span>
                             </div>
                         </div>
 
                         <h2 class="text-1xl font-semibold">
-                            ${product.title}
+                            ${product.title ? product.title : "No Title Available"}
                         </h2>
 
-                        <p class="text-xl font-bold">${product.price}</p>
+                        <p class="text-xl font-bold">${product.price ? product.price : "No Price Available"}</p>
                         <div class="card-actions justify-between pt-2">
                             <button class="btn btn-outline btn-primary h-8 w-32">
                                 <i class="fa-solid fa-eye"></i> Details
@@ -52,10 +52,9 @@ const displayProducts = (products) => {
         `
         productContainer.appendChild(pCard);
     })
-}
+};
 
-
-displaycategories = (data) => {
+const displaycategories = (data) => {   
     const categories = data.map(item => item.category);
     const SetCategories = [...new Set(categories)];
     const container = document.getElementById("all-categories-container");
@@ -64,14 +63,14 @@ displaycategories = (data) => {
     SetCategories.forEach(category => {
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-    <button onclick="filterProducts(\`${category}\`)" 
-    class="btn btn-outline btn-primary rounded">
+    <button onclick="filterProducts(\`${category}\`)"
+    class="btn btn-outline btn-primary rounded-full">
         ${category}
     </button>
-`;;
+`;
         container.appendChild(btnDiv);
     })
-}
+};
 
 
 loadCategories();
