a728a24a88bc8c36c0ef03f68e3aad75de9bacad
Zishan2211 <alljubayerzishan@gmail.com>
2026-02-17 00:11:38 +0600

    add a modal and dynamic

--- script/index.js
+++ script/index.js
@@ -4,19 +4,48 @@ const loadCategories = () => {
         .then((data) => displaycategories(data))
 };
 
+const removeActive = () => {
+    const categoryButtons = document.querySelectorAll(".lesson-btn");
+    categoryButtons.forEach(btn => btn.classList.remove("btn-active"));
+};
+
 const filterProducts = (category) => {
     const url = `https://fakestoreapi.com/products/category/${encodeURIComponent(category)}`;  //I did some research and apply...
     fetch(url)
         .then(res => res.json())
-        .then(data => displayProducts(data))
+        .then(data => {
+            removeActive();
+            const clickBtn = document.getElementById(`btn-category-${category}`);
+            clickBtn.classList.add("btn-active");
+            displayProducts(data)
+        })
 };
+
+const showDetailsModal = (id) => {
+    const url = `https://fakestoreapi.com/products/${id}`
+    displyShowDetailsModal(url);
+}
+
+const displyShowDetailsModal = (data) => {
+    const detailsbox = document.getElementById("details-box");
+    detailsbox.innerHTML = `${data}
+    <div class="modal-action">
+                        <form method="dialog">
+                            <!-- if there is a button in form, it will close the modal -->
+                            <button class="btn">Close</button>
+                        </form>
+                    </div>`
+    document.getElementById("my_modal_5").showModal();
+
+}
+
+
 
 const displayProducts = (products) => {
     const productContainer = document.getElementById("all-products-container");
     productContainer.innerHTML = "";
 
     products.forEach((product) => {
-        console.log(product);
         const pCard = document.createElement("div");
         pCard.innerHTML = `
        <div class="card w-80 bg-base-100 shadow-xl">
@@ -40,7 +69,7 @@ const displayProducts = (products) => {
 
                         <p class="text-xl font-bold">${product.price ? product.price : "No Price Available"}</p>
                         <div class="card-actions justify-between pt-2">
-                            <button class="btn btn-outline btn-primary h-8 w-32">
+                            <button onclick="showDetailsModal('${product.description}')" class="btn btn-outline btn-primary h-8 w-32">
                                 <i class="fa-solid fa-eye"></i> Details
                             </button>
                             <button class="btn btn-primary h-8 w-32">
@@ -54,7 +83,7 @@ const displayProducts = (products) => {
     })
 };
 
-const displaycategories = (data) => {   
+const displaycategories = (data) => {
     const categories = data.map(item => item.category);
     const SetCategories = [...new Set(categories)];
     const container = document.getElementById("all-categories-container");
@@ -63,8 +92,8 @@ const displaycategories = (data) => {
     SetCategories.forEach(category => {
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-    <button onclick="filterProducts(\`${category}\`)"
-    class="btn btn-outline btn-primary rounded-full">
+    <button id="btn-category-${category}" onclick="filterProducts(\`${category}\`)"
+    class="btn btn-outline btn-primary rounded-full lesson-btn">
         ${category}
     </button>
 `;
